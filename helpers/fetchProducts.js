47155f4bdee6eeae7323c1bc93a25f47e5d88de6
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-12 23:28:20 -0300

    Inicia requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (QUERY) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+    const promiseFetch = await fetch(url);
+    const results = await promiseFetch.json();
+
+    return results;
+  } catch (error) {
+    return (new Error('You must provide an url'));
+  } 
 };
 
 if (typeof module !== 'undefined') {
